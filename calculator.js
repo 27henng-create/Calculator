16763e1462b6e5df0a01e3469cd77ae41da493c1
Henn <27HennG@badinhs.org>
2026-03-17 11:36:11 -0400

    working on calc project

--- calculator.js
+++ calculator.js
@@ -2,7 +2,4 @@ const container = document.getElementById("container");
 const display = document.getElementsByClassName("display");
 const clearbtn = document.getElementsByClassName("clear");
 
-function cleardisplay() {
-    document.getElementsByClassName("display") = "";
-}
 // functions detecting what numbers and what operations are being typed
