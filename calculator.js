b7e52c31388a8c3ab1dc50936c9f5ff9ed9f6712
Henn <27HennG@badinhs.org>
2026-03-18 14:26:30 -0400

    worked on clac project

--- calculator.js
+++ calculator.js
@@ -2,4 +2,40 @@ const container = document.getElementById("container");
 const display = document.getElementsByClassName("display");
 const clearbtn = document.getElementsByClassName("clear");
 
-// functions detecting what numbers and what operations are being typed
+let cureentIntput = '', currentOperation = '', previousInput = '';
+
+function appendNumber() {
+    cureentIntput += num;
+    updateDisplay();
+}
+
+function appendOperation() {
+    if (!currentInput) return;
+    if (previousInput) calculate();
+    currentOperation = op;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay();
+}
+
+function calculate() {
+    if (!previousInput || !currentInput) return;
+    let result;
+    const prev = parseFloat(previousInput), curr = parseFloat(currentInput);
+    switch(currentOperation) {
+        case '+': result = prev + curr; break;
+        case '-': result = prev - curr; break;
+        case 'x': result = prec * curr; break;
+        case '/': if (curr === 0) return alert("ERROR Cannot Divide by Zero");
+        result = prev / curr; break 
+    }
+    currentInput = result.toString(display);
+    currentOPeration = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = previousInput = currentOperation = '';
+    updateDisplay();
+}
